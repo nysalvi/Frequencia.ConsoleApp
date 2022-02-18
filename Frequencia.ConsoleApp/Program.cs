b461533bb0401434701a9598f6e91ee50efde709
nysalvi <nysalvi20@gmail.com>
2022-02-18 14:18:44 -0300

    Add project files.

---
+++ Frequencia.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Frequencia.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            int[] numeros = new int[2000];
+            int numeroAtual;
+            string input;
+            while (true) {
+                Console.WriteLine("Digite um número entre 1 e 2000 :\n\tOu digite 's' para sair...");
+                input = Console.ReadLine();
+                if (int.TryParse(input, out numeroAtual) && numeroAtual > 0 && numeroAtual < 2001)
+                    numeros[numeroAtual - 1]++;
+                else if (input.ToLower().Equals("s"))
+                    break;
+            }            
+            for (int i = 0;  i < numeros.Length; i++) {
+                if (numeros[i] > 0)
+                    Console.WriteLine(i + 1 + "aparece " + numeros[i] + "vez(es).");
+            }
+            Console.ReadLine();
+        }
+    }
+}
